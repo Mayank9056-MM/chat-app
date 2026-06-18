b7f5e6679e9ecbe912902b1d854b3d95da9dd362
Mayank Mahajan <mayankmahajan9056@gmail.com>
2026-06-18 19:39:54 +0530

    feat: update environment configuration to include NEXT_PUBLIC_APP_URL for auth client

--- lib/auth-client.ts
+++ lib/auth-client.ts
@@ -1,5 +1,6 @@
-import {createAuthClient} from 'better-auth/react'
+import { config } from "@/config/config";
+import { createAuthClient } from "better-auth/react";
 
 export const authClient = createAuthClient({
-    baseURL: "http://localhost:3000"
-})
+  baseURL: config.NEXT_PUBLIC_APP_URL,
+});
